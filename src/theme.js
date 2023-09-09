866128cd3269567b3fd893527a768038e236d69a
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-09-09 02:45:59 -0300

    :sparkles: feat: add theme page

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    bg: "#004940",
+    text: "#8cc7bf",
+    btn: "#8c4a00",
+    btnHover: "#ffca2c",
+    link: "#cccccc",
+    footer: "#333333",
+    input: "#f8f2f2",
+  },
+  fonts: {
+    primary: "'Poppins', sans-serif",
+  },
+}
+
+export default theme
